05aee0e09b65c549e059ab5a6065ae69c1bb83c3
kuzhifeng113 <kuzhifeng113@sina.com>
2019-03-04 14:01:37 +0800

    测试

--- app/src/main/java/com/woyun/warehouse/bean/TextOneBean.java
+++ app/src/main/java/com/woyun/warehouse/bean/TextOneBean.java
@@ -1,4 +1,13 @@
 package com.woyun.warehouse.bean;
 
 public class TextOneBean {
+    private String haha;
+
+    public String getHaha() {
+        return haha;
+    }
+
+    public void setHaha(String haha) {
+        this.haha = haha;
+    }
 }
